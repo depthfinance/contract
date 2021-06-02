9d6f7479d433e302b6cf0da0059cdb44f591989c
Depth <78601281+depthfinance@users.noreply.github.com>
2021-06-02 14:32:21 +0800

    Update dCompVault.sol

--- depth.vault/dCompVault.sol
+++ depth.vault/dCompVault.sol
@@ -13,15 +13,14 @@ contract dCompVault is ERC20,Ownable,Pausable {
 
     address public want;
     address public husd =0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047;
-    address public usdt =0xa71EdC38d189767582C38A3145b5873052c3e47a;
-    address public wht = 0x5545153CCFcA01fbd7Dd11C0b23ba694D9509A6F;
     address public daoAddress=0xfbaC8c66D9B7461EEfa7d8601568887c7b6f96AD;   //DEP DAO ADDRESS
-    address public usdtSwapAddress=0x07c8689FfC95caf92DA7Cc2A55bCbE7dAFCf0A47; //DEP swap usdt to husd
-    address public husdSwapAddress=0xED7d5F38C79115ca12fe6C0041abb22F0A06C300;//swap compound token to husd
+    //address public usdtSwapAddress=0x07c8689FfC95caf92DA7Cc2A55bCbE7dAFCf0A47; //DEP swap usdt to husd
+    address public husdSwapAddress;//swap compound token to husd
+    uint256 public maxLimit;// max balance limit
     address public cTokenAddress;
     uint256 public balance;//total token balance
-    uint256 public minClaim=10**8;//min interest to claim default 1
-    constructor (address _cTokenAddress) ERC20(
+    uint256 public minClaim=1;//min interest to claim default 1
+    constructor (address _cTokenAddress,address _husdSwapAddress) ERC20(
         string(abi.encodePacked("Depth.Fi Vault ", ERC20(_cTokenAddress).symbol())),
         string(abi.encodePacked("d", ERC20(_cTokenAddress).symbol()))
     ) {
@@ -29,16 +28,14 @@ contract dCompVault is ERC20,Ownable,Pausable {
         require(_cTokenAddress != address(0), "INVALID CTOKEN ADDRESS");
         want  = CToken(_cTokenAddress).underlying();
         cTokenAddress=_cTokenAddress;
+        husdSwapAddress = _husdSwapAddress;
 
     }
     // set min claim interest
     function setMinClaim(uint256 _min)   external onlyOwner{
         minClaim = _min;
     }
-    // set usdt swap contract address
-    function setUsdtSwapAddress(address _address)   external onlyOwner{
-        usdtSwapAddress = _address;
-    }
+
     // set husd swap contract address
     function setHusdSwapAddress(address _address)   external onlyOwner{
         husdSwapAddress = _address;
@@ -49,50 +46,34 @@ contract dCompVault is ERC20,Ownable,Pausable {
         daoAddress = _address;
     }
 
+    // set max deposit limit
+    function setMaxLimit(uint256 _max)   external onlyOwner{
+        maxLimit = _max;
+    }
+
     function swapTokensToHusd(address _token) internal{
+        require(husdSwapAddress!=address(0),"not set husd swap address!");
         uint256 _amount = IERC20(_token).balanceOf(address(this));
         if (_amount==0){
             return;
         }
-        if (_token==usdt){
-            IERC20(usdt).safeApprove(usdtSwapAddress,_amount);
-            UsdtSwapRouter(usdtSwapAddress).exchange_underlying(1,0,_amount,0);
-        }else if(_token!=husd){
-            HusdSwapRouter swap = HusdSwapRouter(husdSwapAddress);
-            address[] memory path1 = new address[](2);
-            path1[0] = _token;
-            path1[1] = husd;
-            uint256 amount1 = swap.getAmountsOut(_amount, path1)[1];
-            uint256 amount2 = 0;
-            address[] memory path2 = new address[](3);
-            path2[0] = _token;
-            path2[1] = wht;
-            path2[2] = husd;
-            if (_token!=wht){
+        IERC20(_token).safeApprove(husdSwapAddress,_amount);
+        IHusdSwap(husdSwapAddress).swapTokensToHusd(_token,_amount);
+    }
 
-                amount2 = swap.getAmountsOut(_amount, path2)[2];
-            }
-            address[] memory path3 = new address[](3);
-            path3[0] = _token;
-            path3[1] = usdt;
-            path3[2] = husd;
-            uint256 amount3 = swap.getAmountsOut(_amount, path3)[2];
-
-            address[] memory path;
-            if (amount1 >= amount2 && amount1 >= amount3) {
-                path = path1;
-            } else if (amount2 >= amount1 && amount2 >= amount3) {
-                path = path2;
-            } else {
-                path = path3;
-            }
-
-            IERC20(_token).safeApprove(husdSwapAddress, _amount);
-            swap.swapExactTokensForTokens(_amount, 0, path, address(this), block.timestamp.add(1800));
+    function isCan() internal view returns(bool){
+        if (cTokenAddress==0x9a57eAB16d371048c56cbE0c4D608096aEC5b405||cTokenAddress==0x3dA74C09ccb8faBa3153b7f6189dDA9d7F28156A){
+            return true;
+        }else{
+            return false;
         }
     }
 
     function harvest() public{
+        harvest(true);
+    }
+
+    function harvest(bool claimCompToken) public{
         //claim interest
         uint256 _selfUnderlying = getSelfUnderlying();
         uint256 _compUnderlyingBalance = IERC20(want).balanceOf(cTokenAddress);
@@ -100,22 +81,33 @@ contract dCompVault is ERC20,Ownable,Pausable {
         if (_interest>=minClaim){
             uint256 _claimAmount = _interest<_compUnderlyingBalance?_interest:_compUnderlyingBalance;
             require(CToken(cTokenAddress).redeemUnderlying(_claimAmount) == 0, "!redeem");
-            swapTokensToHusd(want);
+            if (want!=husd){
+                swapTokensToHusd(want);
+            }
         }
-
+        if (claimCompToken){
         //claim mining token
-        address[] memory markets = new address[](1);
-        markets[0] = cTokenAddress;
-        address _compControlAddress = CToken(cTokenAddress).comptroller();
-        CompControl(_compControlAddress).claimComp(address(this), markets);
-        address _compTokenAddress = CompControl(_compControlAddress).getCompAddress();
-        swapTokensToHusd(_compTokenAddress);
+            address[] memory markets = new address[](1);
+            markets[0] = cTokenAddress;
+            address _compControlAddress = CToken(cTokenAddress).comptroller();
+            address _compTokenAddress;
+            if (isCan()){
+                CompControl(_compControlAddress).claimCan(address(this), markets);
+                _compTokenAddress = CompControl(_compControlAddress).getCanAddress();
+            }else{
+                CompControl(_compControlAddress).claimComp(address(this), markets);
+                _compTokenAddress = CompControl(_compControlAddress).getCompAddress();
+            }
 
+            swapTokensToHusd(_compTokenAddress);
+        }
         //donate husd to dao
         uint256 _husdBalance = IERC20(husd).balanceOf(address(this));
-        IERC20(husd).approve(daoAddress,_husdBalance);
-        //call dao address donate husd
-        IDao(daoAddress).donateHUSD(_husdBalance);
+        if (_husdBalance>0){
+            IERC20(husd).approve(daoAddress,_husdBalance);
+            //call dao address donate husd
+            IDao(daoAddress).donateHUSD(_husdBalance);
+        }
 
 
     }
@@ -130,7 +122,7 @@ contract dCompVault is ERC20,Ownable,Pausable {
     //deposit
     function deposit(uint256 _amount) external whenNotPaused {
         require(_amount>0,"invalid amount");
-
+        require(maxLimit==0||balance.add(_amount)<=maxLimit,"exceed max deposit limit");
         IERC20(want).safeTransferFrom(msg.sender, address(this), _amount);
         //deposit token to compound
         IERC20(want).safeApprove(cTokenAddress, _amount);
@@ -168,6 +160,9 @@ contract dCompVault is ERC20,Ownable,Pausable {
 
     function unpause() external onlyOwner {
         _unpause();
+    }
+    function decimals() public view override returns (uint8) {
+        return ERC20(want).decimals();
     }
 
 }
