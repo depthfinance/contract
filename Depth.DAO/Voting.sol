c3bd394ea1e0566df6c49ef74b61cf3bc71dc43b
dreday <492591061@qq.com>
2021-07-28 10:53:05 +0800

    Update Voting.sol

--- Depth.DAO/Voting.sol
+++ Depth.DAO/Voting.sol
@@ -2,61 +2,61 @@ pragma solidity 0.6.12;
 
 interface ERC20 {
 
-  function totalSupply() external view returns (uint256);
+    function totalSupply() external view returns (uint256);
 
-  function balanceOf(address owner) external view returns (uint256);
+    function balanceOf(address owner) external view returns (uint256);
 
-  function allowance(address owner, address spender) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
 
-  function transfer(address to, uint256 value) external returns (bool);
+    function transfer(address to, uint256 value) external returns (bool);
 
-  function approve(address spender, uint256 value) external returns (bool);
+    function approve(address spender, uint256 value) external returns (bool);
 
-  function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
 
 }
 
 abstract contract Context {
-  function _msgSender() internal view virtual returns (address payable) {
-    return msg.sender;
-  }
+    function _msgSender() internal view virtual returns (address payable) {
+        return msg.sender;
+    }
 
-  function _msgData() internal view virtual returns (bytes memory) {
-    this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
-    return msg.data;
-  }
+    function _msgData() internal view virtual returns (bytes memory) {
+        this; // silence state mutability warning without generating bytecode - see https://github.com/ethereum/solidity/issues/2691
+        return msg.data;
+    }
 }
 
 contract Ownable is Context {
-  address private _owner;
+    address private _owner;
 
-  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-  constructor () internal {
-    address msgSender = _msgSender();
-    _owner = msgSender;
-    emit OwnershipTransferred(address(0), msgSender);
-  }
+    constructor () internal {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
 
-  function owner() public view returns (address) {
-    return _owner;
-  }
+    function owner() public view returns (address) {
+        return _owner;
+    }
 
-  modifier onlyOwner() {
-    require(_owner == _msgSender(), "Ownable: caller is not the owner");
-    _;
-  }
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
 
-  function renounceOwnership() public virtual onlyOwner {
-    emit OwnershipTransferred(_owner, address(0));
-    _owner = address(0);
-  }
+    function renounceOwnership() public virtual onlyOwner {
+        emit OwnershipTransferred(_owner, address(0));
+        _owner = address(0);
+    }
 
-  function transferOwnership(address newOwner) public virtual onlyOwner {
-    require(newOwner != address(0), "Ownable: new owner is the zero address");
-    emit OwnershipTransferred(_owner, newOwner);
-    _owner = newOwner;
-  }
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
 }
 
 contract Voting is Ownable{
@@ -105,20 +105,21 @@ contract Voting is Ownable{
     UserVoting storage userVoting = userVotingMap[msg.sender];
     uint256 totalVotes = 0;
     for (uint16 i = 0; i < userVoting.voteOptionsCount; i ++) {
-        totalVotes += userVoting.votes[i];
+      userVoting.votes[i] = 0;
+      totalVotes += userVoting.votes[i];
     } 
     xDep.transfer(msg.sender, totalVotes);
     userVotingMap[msg.sender] = UserVoting(0, 0);
   }
   
   function vote(uint16 index, uint256 votes) public {
-    // Validation
+    // validation
     require(votingInProcess(), "No voting in process");
     require(!_hasVotesToWithdraw(msg.sender), "Previous votes not withdrawed");
     VotingInfo storage info = votingInfoMap[lastIndex];
     require(info.voteOptionsCount - 1 > index, "Out of options");
     
-    // Update
+    // update
     UserVoting storage userVoting = userVotingMap[msg.sender];
     xDep.transferFrom(msg.sender, address(this), votes);
     if (userVoting.index != lastIndex) {
